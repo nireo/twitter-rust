21c9acbecb2ff55df423962a9f201ea520024ab2
nireo <eemil.lehtikangas1@gmail.com>
2020-07-04 14:35:17 +0300

    added the base http service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(||
+        App::new()
+            .service(index)
+    );
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => server.bind("127.0.0.1:5000")?,
+    };
+
+    server.run().await
+}
+
